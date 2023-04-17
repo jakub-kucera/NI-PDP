efcb254ca438e9a702474e2269e35b7f5d07f964
Jakub Kucera <jakub.kucerja@gmail.com>
2023-04-17 16:02:50 +0200

    more funcitonal HW4

--- tester/main.py
+++ tester/main.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 import os
 from datetime import datetime
@@ -20,83 +21,83 @@ OPEN_MP_PROGRAM_FILE_PATH = "../main_mpi"
 # OPEN_MPI_PROGRAM_FILE_PATH = "../main_mpi"
 
 GRAPHS = [
-    {
-        "path": "graf_10_3.txt",
-        "runtime": 0.0,
-        "rec_calls": "1K",
-        "rec_calls_num": 1000,
-        "max_weight": 1300,
-    },
-    {
-        "path": "graf_10_5.txt",
-        "runtime": 0.0,
-        "rec_calls": "12K",
-        "rec_calls_num": 1200,
-        "max_weight": 1885,
-    },
-    {
-        "path": "graf_10_6.txt",
-        "runtime": 0.02,
-        "rec_calls": "374K",
-        "rec_calls_num": 374000,
-        "max_weight": 2000,
-    },
-    {
-        "path": "graf_12_3.txt",
-        "runtime": 0.0,
-        "rec_calls": "1K",
-        "rec_calls_num": 1000,
-        "max_weight": 1422,
-    },
-    {
-        "path": "graf_12_5.txt",
-        "runtime": 0.0,
-        "rec_calls": "171K",
-        "rec_calls_num": 171000,
-        "max_weight": 2219,
-    },
-    {
-        "path": "graf_12_6.txt",
-        "runtime": 0.0,
-        "rec_calls": "401K",
-        "rec_calls_num": 401000,
-        "max_weight": 2533,
-    },
-    {
-        "path": "graf_12_9.txt",
-        "runtime": 0.47,
-        "rec_calls": "49M",
-        "rec_calls_num": 49000000,
-        "max_weight": 3437,
-    },
-    {
-        "path": "graf_13_9.txt",
-        "runtime": 0.8,
-        "rec_calls": "116M",
-        "rec_calls_num": 116000000,
-        "max_weight": 3700,
-    },
-    {
-        "path": "graf_15_4.txt",
-        "runtime": 0.0,
-        "rec_calls": "502K",
-        "rec_calls_num": 502000,
-        "max_weight": 2547,
-    },
-    {
-        "path": "graf_15_5.txt",
-        "runtime": 0.03,
-        "rec_calls": "1M",
-        "rec_calls_num": 1000000,
-        "max_weight": 2892,
-    },
-    {
-        "path": "graf_15_6.txt",
-        "runtime": 0.05,
-        "rec_calls": "4M",
-        "rec_calls_num": 4000000,
-        "max_weight": 3353,
-    },
+    # {
+    #     "path": "graf_10_3.txt",
+    #     "runtime": 0.0,
+    #     "rec_calls": "1K",
+    #     "rec_calls_num": 1000,
+    #     "max_weight": 1300,
+    # },
+    # {
+    #     "path": "graf_10_5.txt",
+    #     "runtime": 0.0,
+    #     "rec_calls": "12K",
+    #     "rec_calls_num": 1200,
+    #     "max_weight": 1885,
+    # },
+    # {
+    #     "path": "graf_10_6.txt",
+    #     "runtime": 0.02,
+    #     "rec_calls": "374K",
+    #     "rec_calls_num": 374000,
+    #     "max_weight": 2000,
+    # },
+    # {
+    #     "path": "graf_12_3.txt",
+    #     "runtime": 0.0,
+    #     "rec_calls": "1K",
+    #     "rec_calls_num": 1000,
+    #     "max_weight": 1422,
+    # },
+    # {
+    #     "path": "graf_12_5.txt",
+    #     "runtime": 0.0,
+    #     "rec_calls": "171K",
+    #     "rec_calls_num": 171000,
+    #     "max_weight": 2219,
+    # },
+    # {
+    #     "path": "graf_12_6.txt",
+    #     "runtime": 0.0,
+    #     "rec_calls": "401K",
+    #     "rec_calls_num": 401000,
+    #     "max_weight": 2533,
+    # },
+    # {
+    #     "path": "graf_12_9.txt",
+    #     "runtime": 0.47,
+    #     "rec_calls": "49M",
+    #     "rec_calls_num": 49000000,
+    #     "max_weight": 3437,
+    # },
+    # {
+    #     "path": "graf_13_9.txt",
+    #     "runtime": 0.8,
+    #     "rec_calls": "116M",
+    #     "rec_calls_num": 116000000,
+    #     "max_weight": 3700,
+    # },
+    # {
+    #     "path": "graf_15_4.txt",
+    #     "runtime": 0.0,
+    #     "rec_calls": "502K",
+    #     "rec_calls_num": 502000,
+    #     "max_weight": 2547,
+    # },
+    # {
+    #     "path": "graf_15_5.txt",
+    #     "runtime": 0.03,
+    #     "rec_calls": "1M",
+    #     "rec_calls_num": 1000000,
+    #     "max_weight": 2892,
+    # },
+    # {
+    #     "path": "graf_15_6.txt",
+    #     "runtime": 0.05,
+    #     "rec_calls": "4M",
+    #     "rec_calls_num": 4000000,
+    #     "max_weight": 3353,
+    # },
     {
         "path": "graf_15_8.txt",
         "runtime": 1.8,
@@ -129,6 +130,57 @@ GRAPHS = [
 
 
 def main():
+    # process_count = 4
+    # thread_count = 4
+    MAX_PROCESS_COUNT = 6
+    MAX_THREAD_COUNT = 12
+
+    results = {}
+
+
+    for testcase in GRAPHS:
+        print(300 * "#")
+        print(f"RUNNING {testcase['path']},")
+        for process_count, thread_count in itertools.product(range(2, MAX_PROCESS_COUNT+1), range(1, MAX_THREAD_COUNT+1)):
+            print(50 * "=")
+            print(f"Process: {process_count}, Thread: {thread_count}")
+            print(f"RUNNING {testcase['path']},")
+            # print(testcase)
+            graph_path = f"../graphs/{testcase['path']}"
+
+            open_mpi_start = datetime.now()
+            open_mpi_stream_project = os.popen(f"OMP_NUM_THREADS={thread_count} mpirun --allow-run-as-root  -np {process_count} {OPEN_MP_PROGRAM_FILE_PATH} {graph_path} {thread_count}")
+            # open_mp_stream_project = os.popen(f"{OPEN_MP_PROGRAM_FILE_PATH} {graph_path} 4")
+            open_mpi_output = open_mpi_stream_project.read()
+            open_mpi_output_dict = json.loads(open_mpi_output)
+            open_mpi_end = datetime.now()
+            open_mpi_runtime = (open_mpi_end - open_mpi_start)
+
+            if testcase['path'] not in results:
+                results[testcase['path']] = {}
+
+            if process_count not in results[testcase['path']]:
+                results[testcase['path']][process_count] = {}
+
+            results[testcase['path']][process_count][thread_count] = {
+            # {
+                #     "process_count": process_count,
+                #     "thread_count": thread_count,
+                # }:
+                # (process_count, thread_count):
+                "runtime": str(open_mpi_runtime),
+                "max_weight": open_mpi_output_dict['MAX_WEIGHT'],
+                "max_weight_match": open_mpi_output_dict['MAX_WEIGHT'] == testcase['max_weight'],
+            }
+
+            # save dict to JSOn file
+            with open('results.json', 'w') as fp:
+                json.dump(results, fp)
+
+
+
+
+
     print(300 * "#")
     for testcase in GRAPHS:
         print(50*"=")
